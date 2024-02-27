c2e276b1e9d0d536adbb253eb4622004f1bb8e18
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-27 22:00:25 +0100

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-a
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let timerId = null;
+
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.backgroundColor = randomColor;
+    }, 1000);
+});
+
+stopButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
